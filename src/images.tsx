d047152062966bb9829df0d04df8a6be2030b74f
kennedymakokha <katchibo2@gmail.com>
2024-07-27 06:38:27 +0300

    fixing theme color and appending logo

--- src/images.tsx
+++ src/images.tsx
@@ -1,5 +1,5 @@
 import LogoLn from './../src/assets/images/pointex/logoong.png'
 import Logowd from './../src/assets/images/pointex/logowide.png'
-import Logowdbg from './../src/assets/images/pointex/logowide.jpeg'
+// import Logowdbg from './../src/assets/images/pointex/logowide.jpeg'
 
-export { LogoLn, Logowd,Logowdbg }
+export { LogoLn, Logowd }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,14 +6,14 @@ import Attribute from "./dashboardcomponents/attributes"
 import Toprate from "./dashboardcomponents/toprate"
 import Services from './dashboardcomponents/services'
 import Stats from './dashboardcomponents/stats'
-import { Logowdbg } from '../images'
+
 function Dashboard() {
 
 
     return (
         <div className='w-full  flex flex-col  sm:pt-10 py-0'>
-{/* 
-            <img src={Logowdbg} alt="" className="" /> */}
+
+       
             <Main />
             <Attribute />
             <Toprate />
